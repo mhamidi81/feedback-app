2daa255fa4e40bc203eef377842806fcc920b282
mohamed hamidi <mohamed.hamidi@opencellsoft.com>
2022-03-18 16:08:16 +0100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+	let firstName = 'Mohamed';
+	let lastName = 'Hamidi';
+	let color = 'blue';
+	$: name = firstName + ' ' + lastName
+	const toggleColor = () => { 
+		color === 'blue' ? color = 'purple' : color = 'blue'
+		}
+</script>
+
+<main>
+	<h1 style="color: {color}">Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<button on:click={toggleColor} >Click</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
